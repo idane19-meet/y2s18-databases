756ec543b3051f74b4b7626921eb06ce7a760aa9
aboud20-meet <aboud20@meet.mit.edu>
2018-08-09 12:04:41 +0300

    jkfhas

--- exercises/knowledge_databases.py
+++ exercises/knowledge_databases.py
@@ -8,11 +8,17 @@ Base.metadata.create_all(engine)
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
-def add_article():
-	pass
+def add_article(name,age,hobby):
+    student_object = Knowledge(
+        name=name,
+        age=age,
+        hobby=hobby)
+	
+    session.add(student_object)
+    session.commit()
 
 def query_all_articles():
-	pass
+	return session.query(Knowledge).all()
 
 def query_article_by_topic():
 	pass
@@ -21,7 +27,12 @@ def delete_article_by_topic():
 	pass
 
 def delete_all_articles():
-	pass
+	session.query(Knowledge).all().delete()
+	session.commit()
 
 def edit_article_rating():
 	pass
+
+add_article("idan", 16, 'ball')
+delete_all_articles()
+print(query_all_articles())
