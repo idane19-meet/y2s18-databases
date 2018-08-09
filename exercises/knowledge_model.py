0f572cab888849e92b1662967a44c243a96789f8
aboud20-meet <aboud20@meet.mit.edu>
2018-08-09 10:28:02 +0300

    Yoooo

--- exercises/knowledge_model.py
+++ exercises/knowledge_model.py
@@ -6,12 +6,21 @@ from sqlalchemy import create_engine
 Base = declarative_base()
 
 class Knowledge(Base):
-	# Create a table with 4 columns
-	# The first column will be the primary key
-	# The second column should be a string representing
-	# the name of the Wiki article that you're referencing
-	# The third column will be a string representing the 
-	# topic of the article. The last column will be
-	# an integer, representing your rating of the article.
+        # Create a table with 4 columns
+        # The first column will be the primary key
+        # The second column should be a string representing
+        # the name of the Wiki article that you're referencing
+        # The third column will be a string representing the 
+        # topic of the article. The last column will be
+        # an integer, representing your rating of the article.
+        __tablename__ = 'table'
+        guy_id = Column(Integer, primary_key=True)
+        name = Column(String)
+        age = Column(Integer)
+        hobby = Column(String)
 
-	pass
+        def __repr__(self):
+            return("name: {}\n"
+                   "age {}\n"
+                   "hobby: {}\n").format(self.name,self.age,self.hobby)
+a = Knowledge(name='idan',age=16,hobby='ball')
